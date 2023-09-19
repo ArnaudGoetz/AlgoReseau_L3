78f32592d48e5d7e5b119b7069a04c6f6b416ec4
Arnaud GOETZ <arnaud.goetz@etu.unistra.fr>
2023-09-19 09:28:06 +0200

    exo1 valide

--- 01-emetteurUDPv4/sender-udp.c
+++ 01-emetteurUDPv4/sender-udp.c
@@ -14,16 +14,36 @@
 int main (int argc, char *argv [])
 {
     /* test arg number */
-
+    if(argc != 2){
+        fprintf(stderr, "Usage : %s arg number", argv[0]);
+        return EXIT_FAILURE;
+    }
     /* convert and check port number */
+    unsigned short port_number = atoi(argv[1]);
+    if ( port_number < 10000 || port_number > 65000){
+        fprintf(stderr,"Usage : %s port number", argv[0]);
+        return EXIT_FAILURE;
+    }
+        
 
     /* create socket */
 
+    int udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    CHECK(udp_socket);
+
     /* complete sockaddr struct */
+    struct sockaddr_storage ss;
+    struct sockaddr_in *in = (struct sockaddr_in *) &ss;
+    in->sin_addr.s_addr = IP;
+    in->sin_family = AF_INET;
+    in->sin_port = PORT(port_number);
 
     /* send message to remote peer */
+    char buf[] = "hello world";
+    CHECK(sendto(udp_socket,buf, sizeof(buf), 0, (const struct sockaddr *) in, sizeof((*in))));
 
     /* close socket */
+    CHECK(close(udp_socket));
 
     return 0;
 }
