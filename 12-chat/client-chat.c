2491a5cbcbe86391b6092a853ba69e7be8f38a5a
Arnaud GOETZ <arnaud.goetz@etu.unistra.fr>
2023-12-29 13:32:27 +0100

    commentaires

--- 12-chat/client-chat.c
+++ 12-chat/client-chat.c
@@ -18,18 +18,17 @@
 #define SIZE 100
 
 
-
 #ifdef FILEIO
 //--- FICHIER ---
 
-// Function to send a file
+// Fonction pour envoyer le message /FILEpath à un client
 void sendFile(int sockfd, const char *command, struct sockaddr_in6 *local_addr) {
 
     // Extraction du chemin
     char filePath[SIZE];
     sscanf(command, "/FILE%s", filePath);
 
-    // Inform receiver /FILE command
+    // Informe le client de la commande Fichier
     CHECK(sendto(sockfd, "/FILE", strlen("/FILE"), 0, (struct sockaddr *)local_addr, sizeof *local_addr));
 
     int file_fd = open(filePath, O_RDONLY, 0777);
@@ -44,18 +43,17 @@ void sendFile(int sockfd, const char *command, struct sockaddr_in6 *local_addr) 
         CHECK(bytesRead = read(file_fd, buffer, sizeof(buffer)));
 
     }
-
+    // Informe le client de la fin du transfert des données
     CHECK(sendto(sockfd, "/FILE_END", strlen("/FILE_END"), 0, (struct sockaddr *)local_addr, sizeof *local_addr));
     close(file_fd);
 }
 
-// Function to receive a file
+// Fonction pour recevoir les données après une réception de type /FILE
 void receiveFile(int sockfd) {
 
     ssize_t bytesReceived;
     char buffer[SIZE];
     
-    // Assume the file transfer starts with "/FILE" command
     printf("Receiving a file...\n");
 
     int new_file = open("received_file", O_WRONLY| O_CREAT | O_TRUNC, 0666);
@@ -65,8 +63,9 @@ void receiveFile(int sockfd) {
 
     while ( bytesReceived  > 0) {
 
+        // Condition d'arrêt
         if (strncmp(buffer, "/FILE_END", strlen("/FILE_END")) == 0) {
-            printf("File received and saved as received_file.txt\n");
+            printf("File received and saved as received_file \n");
             break;
         }
 
@@ -80,28 +79,29 @@ void receiveFile(int sockfd) {
 
 #endif
 
-#define MAX_USERS 10 // Maximum number of users
-#define SHM_NAME "/my_shared_memory"
+#define MAX_USERS 10 // Nombre de clients maximum
+#define SHM_NAME "/my_shared_memory" // Nom du segment de mémoire partagé
 
 #ifdef USR
+// Structure d'un utilisateur connecté
 struct user{
     int id;
     struct sockaddr_in6 addr;
 };
-
+// Structure principale
 struct Users {
     int nb_connected;
     struct user tab_users[];
 };
 
-// Broadcast function
+// Fonction de Broadcast 
 void broadcastMessage(int sockfd, const char *message, struct Users *shared_users, int index) {
     for (int i = 0; i < shared_users->nb_connected + 1; i++) {
-        // Pas s'envoyer à soi-même le message
+        // On ne veut pas s'envoyer le message
         if (i != index){
             #ifdef FILEIO
             if ( strncmp(message,"/FILE",5) == 0)
-                sendFile(sockfd,message, &shared_users->tab_users[i].addr);
+                sendFile(sockfd,message, &shared_users->tab_users[i].addr); 
             else{
                 sendto(sockfd, message, strlen(message), 0, (struct sockaddr *)&shared_users->tab_users[i].addr, sizeof shared_users->tab_users[i].addr);
             }
@@ -134,11 +134,14 @@ int main(int argc, char *argv[]) {
 
     #ifdef USR
     int index = 0;
+
+    // Création du segment
     int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
     CHECK(shm_fd);
 
-    CHECK(ftruncate(shm_fd, sizeof(struct Users) + MAX_USERS * sizeof(struct user))); // Set the size of the shared memory segment
+    CHECK(ftruncate(shm_fd, sizeof(struct Users) + MAX_USERS * sizeof(struct user))); // On définit la taille du segment
 
+    // On fait une projection du segment
     struct Users *shared_users = mmap(NULL, sizeof(struct Users) + MAX_USERS * sizeof(struct user), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
     #endif
 
@@ -163,25 +166,25 @@ int main(int argc, char *argv[]) {
     
     int connected = 0;
 
-    /* Check if a client is already present */
+    // On check si un client est déja connecté
     if (bind(sockfd, (struct sockaddr *)local_addr, sizeof *local_addr) == -1) {
         if (errno == EINVAL || errno == EADDRINUSE) {
             #ifdef USR
-            // besoin d'envoyer a un seul client ! ici local
+            // On informe 1 user et on incrémente 
             shared_users->nb_connected++;
             index = shared_users->nb_connected;
             #endif
             CHECK(sendto(sockfd, "/HELO", strlen("/HELO"), 0, (struct sockaddr *)local_addr, sizeof *local_addr));
         }
     } else {
-        // Case when no client is present
-
-        // Initialisation du premier !
+        // Cas ou on est le premier connecté 
+        
         #ifdef USR
+        // Initialisation du premier client dans la mémoire partagée!
         shared_users->nb_connected = 0;
         if (shared_users->nb_connected < MAX_USERS) {
-                        shared_users->tab_users[shared_users->nb_connected].id = shared_users->nb_connected;
-                        memcpy(&shared_users->tab_users[shared_users->nb_connected].addr, local_addr,
+                        shared_users->tab_users[shared_users->nb_connected].id = shared_users->nb_connected; // index 0 dans le tableau users
+                        memcpy(&shared_users->tab_users[shared_users->nb_connected].addr, local_addr,        // set de l'adresse
                                sizeof(struct sockaddr_in6));
                     }
         #endif
@@ -192,14 +195,13 @@ int main(int argc, char *argv[]) {
         socklen_t sender_addr_len = sizeof(sender_ss);
 
         while (!connected) {
-            // Wait for incoming message
+            // On attend un message de type /HELO
             ssize_t bytes_received = recvfrom(sockfd, buffer, SIZE - 1, 0, (struct sockaddr *)sender_addr, &sender_addr_len);
             CHECK(bytes_received);
 
             if (bytes_received > 0) {
                 buffer[bytes_received] = '\0';
                 if (strncmp(buffer, expectedMessage, 5) == 0) {
-
 
                     char host[NI_MAXHOST];
                     char serv[NI_MAXSERV];
@@ -212,18 +214,17 @@ int main(int argc, char *argv[]) {
                     printf("%s %s\n",host,serv);
 
                     #ifdef USR
-                    // Add the user to the shared memory segment
+                    // Si on reçoit /HELO et qu'il y a de la place, on l'ajoute en tant qu'utilisateur connecté
                     if (shared_users->nb_connected < MAX_USERS) {
                         shared_users->tab_users[shared_users->nb_connected].id = shared_users->nb_connected;
                         memcpy(&shared_users->tab_users[shared_users->nb_connected].addr, sender_addr,
                                sizeof(struct sockaddr_in6));
-                        //printf("nb_connected attente 1: %d\n", shared_users->nb_connected);
                     }
                     #else
                     memcpy(local_addr, sender_addr, sizeof(*sender_addr));
                     #endif
                     
-                    connected = 1;
+                    connected = 1; // sortie de la boucle
                 } 
                 else {
                     printf("Received message is not /HELO. Continuing to wait for the correct message...\n");
@@ -240,51 +241,51 @@ int main(int argc, char *argv[]) {
     fds[1].fd = sockfd;     
     fds[1].events = POLLIN; 
 
-    //char buffer2[SIZE];
     // Main loop
     while (1) {
 
         int result = poll(fds, 2, -1); 
         CHECK(result);
 
-        // user input
+        // Input de l'utilisateur 
         if (fds[0].revents & POLLIN) 
         {
             int r = read(0,buffer, SIZE);
             CHECK(r);
             buffer[r] = '\0';
+            // Gestion de la compilation conditionnelle
             #ifdef FILEIO
-                #ifdef USR      // Combinaison des deux
+                #ifdef USR      // Combinaison USR + FILEIO
                  if (strncmp(buffer,"/HELO",5) != 0 && strncmp(buffer, "/QUIT", 5) != 0)
-                    broadcastMessage(sockfd,buffer, shared_users, index);
+                    broadcastMessage(sockfd,buffer, shared_users, index);       // On envoie à tout le monde
                 #else           // Seulement FILEIO
                 if ( strncmp(buffer,"/FILE",5) == 0){
-                    sendFile(sockfd, buffer,local_addr);
+                    sendFile(sockfd, buffer,local_addr);                        // On envoie au second utilisateur la commande /FILE
                 }
                 else{
+                    // Cas DATA, on envoie juste les données tapées
                     CHECK(sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)local_addr, sizeof *local_addr));
                 }
                 #endif  
             #else   // Seulement USR
                 #ifdef USR
-                if (strncmp(buffer,"/HELO",5) != 0 && strncmp(buffer, "/QUIT", 5) != 0)
-                    broadcastMessage(sockfd,buffer,shared_users, index);
+                if (strncmp(buffer,"/HELO",5) != 0 && strncmp(buffer, "/QUIT", 5) != 0) // DATA "normale"
+                    broadcastMessage(sockfd,buffer,shared_users, index);           // On envoie à tout le monde le message
                 #else
-
+                // Cas aucune compilation conditionelle
                 CHECK(sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)local_addr, sizeof *local_addr));
                 #endif
-            #endif
-
-
+            #endif  
+            
+            // on sort de la boucle
             if (strncmp(buffer, "/QUIT", 5) == 0) 
                 break; 
 
         }
 
-        // socket 
+        // Données dans le socket
         if (fds[1].revents & POLLIN) 
         {   
-            //printf("Entrée écoute\n");
 
             struct sockaddr_storage sender_ss;
             struct sockaddr_in6 *sender_addr = (struct sockaddr_in6 *)&sender_ss;
@@ -303,10 +304,10 @@ int main(int argc, char *argv[]) {
                 }
                 #ifdef FILEIO
                 else if (strncmp(buffer, "/FILE", 5) == 0) {
-                    receiveFile(sockfd);
+                    receiveFile(sockfd);    // Cas FILE, on reçoit et écrit les données
                 }
                 #endif
-                else if (strncmp(buffer, "/HELO",5) == 0){
+                else if (strncmp(buffer, "/HELO",5) == 0){ // Cas nouveau utilisateur détecté
                 
                     int err = getnameinfo((struct sockaddr *)sender_addr, sender_addr_len, host2, NI_MAXHOST, serv2, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
                     if (err != 0) {
@@ -315,24 +316,23 @@ int main(int argc, char *argv[]) {
                     }
 
                     #ifdef USR
-                    // Add the user to the shared memory segment
+                    // On ajoute le nouvel utilisateur si il y a la place
                     if (shared_users->nb_connected < MAX_USERS) {
                         shared_users->tab_users[shared_users->nb_connected].id = shared_users->nb_connected;
                         memcpy(&shared_users->tab_users[shared_users->nb_connected].addr, sender_addr,
                                sizeof(struct sockaddr_in6));
-                        //printf("nb_connected socket : %d\n", shared_users->nb_connected);
-                        
                     }
                     #endif
                 }
                 else {
+                    // Affichage des messages reçus et identification de la source avec le port
                     printf("Utilsateur port %d : %s", htons(sender_addr->sin6_port), buffer);
                 }
             }
         }
     }
 
-    // Close the socket
+    // Close the socket / Memory
     close(sockfd);
 
     #ifdef USR
@@ -340,9 +340,8 @@ int main(int argc, char *argv[]) {
     // Unmap 
     CHECK(munmap(shared_users, sizeof(struct Users) + MAX_USERS * sizeof(struct user)));
     CHECK(close(shm_fd));
-    // Unlink 
-    CHECK(shm_unlink(SHM_NAME));
 
     #endif
+
     return 0;
 }
